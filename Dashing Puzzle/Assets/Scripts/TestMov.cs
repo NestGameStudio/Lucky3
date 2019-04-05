5673c68b90615d8fc2c3e3296bf6b76d856007dc
Bernardo Gadelha <bernardompg@gmail.com>
2019-04-05 18:19:44 -0300

    Test Scene para Mov

---
+++ Dashing Puzzle/Assets/Scripts/TestMov.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TestMov : MonoBehaviour
+{
+    public Tilemap Ground;
+    public GameObject Spawn;
+
+    Vector3Int spawnCellPosition;
+    Vector3Int currentPlayerCellPosition;
+    TileBase currentPlayerPositionTileBase;
+
+    Rigidbody2D playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = this.GetComponent<Rigidbody2D>();
+
+        // Inicia o jogador na posição de spawn
+        spawnCellPosition = Ground.WorldToCell(Spawn.transform.position);
+        this.transform.position = Ground.GetCellCenterWorld(spawnCellPosition);
+
+        currentPlayerCellPosition = Ground.WorldToCell(this.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3Int nextPosition = currentPlayerCellPosition;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            nextPosition += new Vector3Int(0, 1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            nextPosition -= new Vector3Int(0, 1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            nextPosition -= new Vector3Int(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            nextPosition += new Vector3Int(1, 0, 0);
+        }
+
+        // Checa se é um tile acessível para movimentação
+        TileBase nextTile = Ground.GetTile(nextPosition);
+        Debug.Log(nextTile + " KD");
+        Debug.Log(nextPosition);
+
+        currentPlayerCellPosition += nextPosition;
+        currentPlayerPositionTileBase = nextTile;
+        this.transform.position = Ground.GetCellCenterWorld(currentPlayerCellPosition);
+
+        
+    }
+
+}
