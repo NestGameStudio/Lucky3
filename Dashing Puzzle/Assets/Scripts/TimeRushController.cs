c6f4f865e61ef2e8ca89cc564184a5e1f836ca8e
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2019-04-22 11:05:47 -0300

    HOLDISBARRINHA

---
+++ Dashing Puzzle/Assets/Scripts/TimeRushController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeRushController : MonoBehaviour
+{
+    // Preencher a barrinha de tempo
+    // Pegar a pocentagem da posicao do player nos levels, atualizar por level se possivel atualizar no update
+    // pegar numero de levels e trabalhar a completude deles em porcentagem, se possivel ver a 
+    // distancia do player da porta que leva ao final do jogo e ver a porcentagem
+    // dele conforme a aproximacao dessa porta
+    // pegar numero de levels
+    // pegar posicao do player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
