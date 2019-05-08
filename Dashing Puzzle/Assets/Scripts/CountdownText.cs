a21416cba3295ebf969a498b6f8f9956f94208d8
lucas3311 <lucaslimareis3311@gmail.com>
2019-05-07 21:00:18 -0300

    Timer

---
+++ Dashing Puzzle/Assets/Scripts/CountdownText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownText : MonoBehaviour
+{
+    public GameObject gameManager;
+    public int SecondsToCompletion = 60; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //currentTime / SecondsToCompletion
+        //float time = gameManager.GetComponent<TimeRushController>().LittleBar.value - gameManager.GetComponent<TimeRushController>().currentTime;
+        float time = SecondsToCompletion - gameManager.GetComponent<TimeRushController>().currentTime;
+        gameObject.GetComponent<Text>().text = (time).ToString();
+    }
+}
