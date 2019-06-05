d36eb2d7ef6dbe886d65ea305a32c8845b86ba59
misantiago17 <michelle.santiago10@gmail.com>
2019-06-05 14:40:15 -0300

    painel na victory scene

---
+++ Dashing Puzzle/Assets/Scripts/SaveNewHighscore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveNewHighscore : MonoBehaviour
+{
+    // vinculado ao painel
+    // ele vai receber o tempo restante no qual ojogador zerou o jogo
+    // se ele for menor que todos os tempos registrados ele só mostra a tabela de highscore
+    // se ele for maior que algum deles ele substitui a célula
+    // se ele for igual substitui pra cima 
+
+    // primeiro ele mostra todos os recordes salvos
+    // depois ele avalia como descrito acima
+    // depois ele salva ou não
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
